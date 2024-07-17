195d7aaf5e99564d099ceb4f916e0fcc3c022f7c
Gopeshwar Kumar <gopeshwarkumark@gmail.com>
2024-07-17 23:21:50 +0530

    first committ

---
+++ weather.js
@@ -0,0 +1,41 @@
+
+            
+
+            function getweather(){
+                let city=document.getElementById("city").value;
+                let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8bd991c12ee11646e3b2ed07ee7b03b5`;
+            
+            fetch(url)
+            .then((res)=>res.json())
+            .then((data)=>{
+                document.getElementById("x1").innerHTML=data.main.temp+" K";
+                document.getElementById("x2").innerHTML=data.main.pressure+" mmHg";
+                document.getElementById("x3").innerHTML=data.main.humidity;
+                document.getElementById("x4").innerHTML=data.wind.speed+" m/s";
+                console.log(data);
+                console.log(data.weather.main);
+            }
+                );
+                }
+            //    let url=`https://api.openweathermap.org/data/2.5/weather?q=gopalganj&appid=8bd991c12ee11646e3b2ed07ee7b03b5`;
+            
+            // fetch(url).then((res)=>res.json())
+            // .then((data)=>console.log(data));
+            
+            
+            gsap.from("h2",{
+                y:20,
+                duration:2,
+            })
+            gsap.to("#btn",{
+                x:10,
+                duration:1,
+            })
+            
+            // let ball=document.querySelector("#box");
+            // ball.addEventListener("mousemove",function(dets){
+            //     gsap.to("#box",{
+            //         y:dets.y,
+            //         x:dets.x,
+            //     });
+            // })
