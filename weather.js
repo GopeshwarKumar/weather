f04eb0a62382b46daa6babd86ca44eff1c146a10
Gopeshwar Kumar <gopeshwarkumark@gmail.com>
2024-07-24 00:33:34 +0530

    part

--- weather.js
+++ weather.js
@@ -1,27 +1,25 @@
 
             
 
-            function getweather(){
-                let city=document.getElementById("city").value;
-                let url=`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8bd991c12ee11646e3b2ed07ee7b03b5`;
+function getweather() {
+    let city = document.getElementById("city").value;
+    let url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=8bd991c12ee11646e3b2ed07ee7b03b5`;
             
-            fetch(url)
-            .then((res)=>res.json())
-            .then((data)=>{
-                document.getElementById("x1").innerHTML=data.main.temp+" K";
-                document.getElementById("x2").innerHTML=data.main.pressure+" mmHg";
-                document.getElementById("x3").innerHTML=data.main.humidity;
-                document.getElementById("x4").innerHTML=data.wind.speed+" m/s";
-                console.log(data);
-                console.log(data.weather.main);
-            }
-                );
-                }
+    fetch(url)
+        .then((res) => res.json())
+        .then((data) => {
+            document.getElementById("x1").innerHTML = data.main.temp + " K";
+            document.getElementById("x2").innerHTML = data.main.pressure + " mmHg";
+            document.getElementById("x3").innerHTML = data.main.humidity;
+            document.getElementById("x4").innerHTML = data.wind.speed + " m/s";
+            console.log(data);
+        }
+        );
+}
             //    let url=`https://api.openweathermap.org/data/2.5/weather?q=gopalganj&appid=8bd991c12ee11646e3b2ed07ee7b03b5`;
             
             // fetch(url).then((res)=>res.json())
             // .then((data)=>console.log(data));
-            
             
             gsap.from("h2",{
                 y:20,
@@ -29,7 +27,8 @@
             })
             gsap.to("#btn",{
                 x:10,
-                duration:1,
+                duration: 1,
+                scale: 1.3,
             })
             
             // let ball=document.querySelector("#box");
